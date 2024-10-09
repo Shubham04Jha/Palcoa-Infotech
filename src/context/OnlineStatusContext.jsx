0ff6b702ea8f7a760209323320ac64fec3bc777a
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-10-09 13:14:14 +0530

    Custom useSendData hook added

--- src/context/OnlineStatusContext.jsx
+++ src/context/OnlineStatusContext.jsx
@@ -1,9 +1,8 @@
 import React, { createContext, useContext, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import useOnlineStatus from '../hooks/useOnlineStatus';
-import { sendDataWhenOnlineFailure, sendDataWhenOnlineStart, sendDataWhenOnlineSuccess } from '../redux/user/userSlice';
 import { db } from '../database/dexie';
 import { useLiveQuery } from 'dexie-react-hooks';
+import useSendData from '../hooks/useSendData';
 
 const OnlineStatusContext = createContext();
 
@@ -12,51 +11,13 @@ export const useOnlineStatusProvider = () => {
 };
 
 export const OnlineStatusProvider = ({ children }) => {
-    const dispatch = useDispatch();
     const isOnline = useOnlineStatus();
     const products = useLiveQuery(() => db.products.toArray());
+    const sendData = useSendData(isOnline, products);
 
     useEffect(() => {
-        if (isOnline && products && products.length > 0) {
-            const dataToSend = {
-                data: products,
-            };
-
-            const sendData = async () => {
-                dispatch(sendDataWhenOnlineStart());
-                try {
-                    const response = await fetch('http://localhost:3000/send', {
-                        method: 'POST',
-                        body: JSON.stringify(dataToSend),
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                    });
-
-                    console.log(dataToSend);
-                    console.log("Status code:", response.status);
-
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-
-                    await response.json();
-
-                    if (response.status === 201) {
-                        console.log("Received status 201, deleting products");
-                        await db.products.clear();
-                    }
-
-                    dispatch(sendDataWhenOnlineSuccess());
-                } catch (error) {
-                    dispatch(sendDataWhenOnlineFailure(error.message));
-                    console.error("Error sending data:", error);
-                }
-            };
-
-            sendData();
-        }
-    }, [isOnline, products, dispatch]);
+        sendData();
+    }, [isOnline, products, sendData]);
 
     return (
         <OnlineStatusContext.Provider value={isOnline}>
---
+++ src/hooks/useSendData.js
@@ -0,0 +1,50 @@
+import { useDispatch } from 'react-redux';
+import { sendDataWhenOnlineFailure, sendDataWhenOnlineStart, sendDataWhenOnlineSuccess } from '../redux/user/userSlice';
+import { db } from '../database/dexie';
+
+const useSendData = (isOnline, products) => {
+    const dispatch = useDispatch();
+
+    const sendData = async () => {
+        if (isOnline && products && products.length > 0) {
+            const dataToSend = {
+                data: products,
+            };
+
+            dispatch(sendDataWhenOnlineStart());
+
+            try {
+                const response = await fetch('http://localhost:3000/send', {
+                    method: 'POST',
+                    body: JSON.stringify(dataToSend),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+
+                console.log(dataToSend);
+                console.log("Status code:", response.status);
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                await response.json();
+
+                if (response.status === 201) {
+                    console.log("Received status 201, deleting products");
+                    await db.products.clear();
+                }
+
+                dispatch(sendDataWhenOnlineSuccess());
+            } catch (error) {
+                dispatch(sendDataWhenOnlineFailure(error.message));
+                console.error("Error sending data:", error);
+            }
+        }
+    };
+
+    return sendData;
+};
+
+export default useSendData;
